44d9e999fa26d3cdcb5600f8871c98dab9045a6c
SomeoneAlt-86 <76504556+SomeoneAlt-86@users.noreply.github.com>
2022-10-21 17:11:49 +0530

    Add files via upload

---
+++ Windows_setup.ps1
@@ -0,0 +1,62 @@
+# Name is Important
+echo "
+                #####    #     #    #####    #     #   #     # 
+               #     #   ##    #   #     #   #     #   ##    # 
+               #         # #   #   #         #     #   # #   # 
+                #####    #  #  #   #  ####   #  #  #   #  #  # 
+                     #   #   # #   #     #   #  #  #   #   # # 
+               #     #   #    ##   #     #   #  #  #   #    ## 
+                #####    #     #    #####     ## ##    #     # 
+"
+
+# Set Wget Progress to Silent, Becuase it slows down Downloading by +50x
+echo "Setting Wget Progress to Silent, Becuase it slows down Downloading by +50x`n"
+$ProgressPreference = 'SilentlyContinue'
+
+
+#Use choco for installation
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+choco install openjdk11
+
+
+# Downloading Burp Suite Professional
+if (Test-Path Burp-Suite-Pro.jar){
+    echo "Burp Suite Professional JAR file is available.`nChecking its Integrity ...."
+    if (((Get-Item Burp-Suite-Pro.jar).length/1MB) -lt 500 ){
+        echo "`n`t`tFiles Seems to be corrupted `n`t`tDownloading Latest Burp Suite Professional ...."
+        wget "https://portswigger-cdn.net/burp/releases/download?product=pro&version=&type=jar" -O "Burp-Suite-Pro.jar"
+        echo "`nBurp Suite Professional is Downloaded.`n"
+    }else {echo "File Looks fine. Lets proceed for Execution"}
+}else {
+    echo "`n`t`tDownloading Latest Burp Suite Professional ...."
+    wget "https://portswigger-cdn.net/burp/releases/download?product=pro&version=&type=jar" -O Burp-Suite-Pro.jar
+    echo "`nBurp Suite Professional is Downloaded.`n"
+}
+
+# Creating Burp.bat file with command for execution
+if (Test-Path burp.bat) {rm burp.bat} 
+$path = "java --add-opens=java.desktop/javax.swing=ALL-UNNAMED --add-opens=java.base/java.lang=ALL-UNNAMED -javaagent:`"$pwd\loader.jar`" -noverify -jar `"$pwd\Burp-Suite-Pro.jar`""
+$path | add-content -path Burp.bat
+echo "`nBurp.bat file is created"
+
+
+# Creating Burp-Suite-Pro.vbs File for background execution
+if (Test-Path Burp-Suite-Pro.vbs) {
+   Remove-Item Burp-Suite-Pro.vbs}
+echo "Set WshShell = CreateObject(`"WScript.Shell`")" > Burp-Suite-Pro.vbs
+add-content Burp-Suite-Pro.vbs "WshShell.Run chr(34) & `"$pwd\Burp.bat`" & Chr(34), 0"
+add-content Burp-Suite-Pro.vbs "Set WshShell = Nothing"
+echo "`nBurp-Suite-Pro.vbs file is created."
+
+# Remove Additional files
+rm Kali_Linux_Setup.sh
+del -Recurse -Force .\.github\
+
+
+# Lets Activate Burp Suite Professional with keygenerator and Keyloader
+echo "Reloading Environment Variables ...."
+$env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User") 
+echo "`n`nStarting Keygenerator ...."
+start-process java.exe -argumentlist "-jar keygen.jar"
+echo "`n`nStarting Burp Suite Professional"
+java --add-opens=java.desktop/javax.swing=ALL-UNNAMED --add-opens=java.base/java.lang=ALL-UNNAMED -javaagent:"loader.jar" -noverify -jar "Burp-Suite-Pro.jar"
