70f63d6b4abdacb89b95ba6edd2f167d5c38b95e
SomeoneAlt-86 <76504556+SomeoneAlt-86@users.noreply.github.com>
2022-10-21 17:12:43 +0530

    Update Windows_setup.ps1

--- Windows_setup.ps1
+++ Windows_setup.ps1
@@ -18,6 +18,7 @@ $ProgressPreference = 'SilentlyContinue'
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 choco install openjdk11
 
+echo "Java installed"
 
 # Downloading Burp Suite Professional
 if (Test-Path Burp-Suite-Pro.jar){
